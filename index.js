922f7b89f843ca265220a8cffe3952f1eb9a7267
Rico Sta. Cruz <hi@ricostacruz.com>
2013-10-11 14:44:18 +0800

    Docs.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var defer = module.exports = function(fn) {
   // Return a function that decorates the original
   return function() {
-    var ok, fail;
     var self = this;
     var last = arguments[arguments.length-1];
-    var args = [].slice.call(arguments, 0, arguments.length-1);
+
+    // These are handlers that will be populated, depending on whether it was
+    // invoked async-style or promise-style.
+    var ok, fail;
 
     // Create the new callback.
+    // This is an overloaded function:
+    //
+    //   - When called with an `Error` instance, it reports it to the "fail" handler.
+    //
+    //   - When called with a function, it wraps that in a try/catch block,
+    //     and reports any errors back to the fail handler.
+    //
+    //   - Everything else is reported to the "ok" handler.
+    //
     var next = function(result) {
       var _self = this;
       if (result instanceof Function) {
@@ -24,12 +35,17 @@ var defer = module.exports = function(fn) {
       }
     };
 
-    // Swap out the last arg for a new callback
+    // This function will invoke the given `fn`, swapping out the last arg for
+    // a new callback. If `fn` returns a promise, treat it accordingly. If `fn`
+    // throws an error, report it to the fail handler.
+    //
+    var args = [].slice.call(arguments, 0, arguments.length-1);
     var invoke = function() {
       try {
         var result = fn.apply(self, args.concat([next]));
         if (result && result.then)
           result.then(ok, function(err) { return fail(errify(err)); });
+        return result;
       } catch (err) {
         fail(err);
       }
@@ -37,19 +53,19 @@ var defer = module.exports = function(fn) {
 
     // Used as an async:
     // The function was invoked with a callback at the `last` argument.
+    //
     if (typeof last === 'function') {
       var callback = last;
       fail = function(err) {
-        callback.call(self, err);
-      };
+        callback.call(self, err); };
       ok = function(result) {
-        callback.apply(self, [undefined].concat([].slice.call(arguments)));
-      };
-      invoke();
-      return;
+        callback.apply(self, [undefined].concat([].slice.call(arguments))); };
+      return invoke();
     }
 
-    // Used as a promise
+    // Used as a promise:
+    // The function was invoked without a callback; ensure that it returns a promise.
+    //
     else {
       var p = {};
       var promise = defer.promise(function(_ok, _fail) { p.ok = _ok; p.fail = _fail; });
